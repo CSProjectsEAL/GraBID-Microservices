ccae22c1bd57d9e43a8ceb06b6aeed9c686f32b2
alex855k <alexander2341@gmail.com>
2018-11-27 14:55:35 +0100

    (#12) Changed id as an int to name as a unique id

--- Src/Ingestion/IngestionSource.cs
+++ Src/Ingestion/IngestionSource.cs
@@ -6,7 +6,7 @@ namespace Ingestion
 {
     public class IngestionSource 
     {
-        public int id { get; set; }
+        public string name { get; set; }
         public string ApiUrl { get; set; }
         public string Credential { get; set; }
         public string ForwardMessageQueue { get; set; }
