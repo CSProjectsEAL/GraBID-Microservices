2b626f14b2e7b9c9d093d0eea07b439a8e116079
alex855k <alexander2341@gmail.com>
2018-11-27 14:44:24 +0100

    (#12) Add capability to input argument for the particular type of ingestion source the cronjob should go to

--- Src/Ingestion/Program.cs
+++ Src/Ingestion/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
 using RabbitMQ.Client;
+using Serilog;
 using Shared;
 using System;
+using System.Threading;
 
 namespace Ingestion
 {
@@ -9,10 +11,29 @@ namespace Ingestion
     {
         static void Main(string[] args)
         {
-            //
+            ILogger logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            string sourceID = "";
+            /*
+            try {
+                sourceID = args[0];
+            } catch (Exception e) {
+                logger.Error("Incorrect argument for ingestion dll crontab: " + e.Message);
+            }
+            */
+            sourceID = "ResellerData";
+            if (sourceID != ""){
+                IngestionService ingestService = new IngestionService(logger);
 
-
-            IngestionService ingestService = new IngestionService();
+                try {
+                    while (true) { 
+                        ingestService.Ingest(sourceID);
+                        Thread.Sleep(3000);
+                    }
+                } catch(Exception e)
+                {
+                    logger.Error("Error: " + e.Message + "\n" + "Exception trace: " + e.StackTrace);
+                }
+            }
         }
 
     }
