72d46c65353d2a26ca662bb1cba8c0a1a4a4bea4
alex855k <alexander2341@gmail.com>
2018-12-13 17:51:11 +0100

    (Refiner-service-CSProjectsEAL/GraBID#11) Add MongoDB Driver dependency and refactored refiner service file.

--- Src/Refiner/RefinerService.cs
+++ Src/Refiner/RefinerService.cs
@@ -1,88 +1,83 @@
-﻿using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using Shared;
-using System.Collections.Generic;
+﻿using System;
 using System.Text;
-using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Serilog;
+using Shared;
 
 namespace Refiner
 {
     public class RefinerService
     {
-        private static string exchange = "grabid_exchange";
-        private static string routingKey = "mono.data.received";
-        private static IDBFacade _db;
-        // Definition of sources and their processors
-        private static IDictionary<string, IList<IDataProcessor>> _dataSources;
+        private ILogger _logger;
+        private static IConnection _conn;
+        private static IModel _channel;
+        private const string Exchange = "mono.data.received ";
 
-        private static IConnection conn;
-
-        private static IModel channel;
-
-        private static readonly AutoResetEvent WaitHandle = new AutoResetEvent(false);
-
-        public RefinerService(IDBFacade db)
+        public RefinerService(ILogger logger, IConnectionFactory factory)
         {
-            _dataSources = dataSources;
-            _db = db;
+            _logger = logger;;
+            _conn = factory.CreateConnection();
+            _channel = _conn.CreateModel();
         }
-        // Listen to queues
-        public static void StartListening()
-        {
-            channel.ExchangeDeclare(exchange: exchange, type: "topic");
-
-
-            var queueName = channel.QueueDeclare().QueueName;
-
-            channel.QueueBind(queue: queueName,
-                                  exchange: exchange,
-                                  routingKey: routingKey);
-
-            var consumer = new EventingBasicConsumer(channel);
-            
-        }
-
-
-        // Establish connection
-        public void ConnectToRabbitMQ() {
-            var factory = new ConnectionFactory() { HostName = "rabbit.docker" };
-            conn = factory.CreateConnection();
-            channel = conn.CreateModel();
-        }
-
-        // Process data
-        public void ProcessData() {
-
-            consumer.Received += (model, ea) =>
-            {
-            var body = ea.Body;
-            var message = Encoding.UTF8.GetString(body);
-            var key = ea.RoutingKey;
-            Log.Information($" [x] Received '{key}':'{message}'");
-            var envelope = JsonConvert.DeserializeObject<Envelope<string>>(message);
-
-            dynamic json = JObject.Parse(envelope.Payload);
-            string messageString = json.message;
-            string userString = json.users;
-            string[] userArray = userString.Split(",");
-            string[] messages = userArray.Select(user => { return $"{messageString} {user}"; }).ToArray();
-            var returnEnvelope = new Envelope<string[]>(envelope.Id, messages);
-            string newMessage = JsonConvert.SerializeObject(returnEnvelope);
-            byte[] messageBodyBytes = Encoding.UTF8.GetBytes(newMessage);
-
-
-
-            channel.BasicPublish(exchange, "mono.data.refined", null, messageBodyBytes);
-            Log.Information($" [x] Sent 'mono.data.refined':'{newMessage}'");
-
-            };
-
-        }
-
-        // Store data, when a new batch of data is received
-        public void StoreData() {
-
         
+        public void RetrieveMessageFromQueue (string queue)
+        {
+            using (_conn)
+            {
+                using (_channel)
+                {
+                    _channel.QueueDeclare(queue, true, false, false, null);
+
+                    _channel.ExchangeDeclare(Exchange, "fanout");
+
+                    _channel.QueueBind(queue, Exchange, "");
+
+                    var consumer = new EventingBasicConsumer(_channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var key = ea.RoutingKey;
+                        Console.WriteLine($" [x] Received '{key}':'{message}'");
+                        var envelope = JsonConvert.DeserializeObject<Envelope<string>>(message);
+
+                        var processor = new SubscriptionDataProcessor(envelope);
+                        var cleanData = processor.CleanData();
+                        
+                        ForwardMessageToQueue("mono.data.refined", cleanData);
+                        
+                        Console.WriteLine($" [x] Sent 'mono.data.refined':'{cleanData}'");
+                    };
+                    
+                    _channel.BasicConsume(queue, true, consumer);
+                }
+            }
+        }
+
+        public void ForwardMessageToQueue (string queue, JToken cleanData)
+        {
+            var message = JsonConvert.SerializeObject(cleanData);
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes(message);
+            
+            using (_conn)
+            {
+                using (_channel)
+                {
+                    _channel.QueueDeclare(queue, true, false, false, null);
+                    
+                    _channel.ExchangeDeclare(Exchange, "fanout");
+                    
+                    _channel.QueueBind(queue, Exchange, "");
+
+                    _channel.BasicPublish(queue, "", _channel.CreateBasicProperties(), messageBodyBytes);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", Exchange, message);
+                }
+            }
+            
         }
     }
 }
