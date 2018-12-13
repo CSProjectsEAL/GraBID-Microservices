0840e5d0862ccb53b54ff169bcae40e36d3133de
alex855k <alexander2341@gmail.com>
2018-12-13 18:07:15 +0100

    (Refiner-service-CSProjectsEAL/GraBID#11) Refactor classes to logical folders and namespaces

---
+++ Src/Refiner/DB/IDBFacade.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Refiner
+{
+    public interface IDBFacade
+    {
+        void SaveCollection(JArray collection, string collectionName, string databaseName);
+        string FetchCollection(string collectionName, string databaseName);
+        string GetLatestID(string id, string collectionName, string databaseName);
+    }
+}
---
+++ Src/Refiner/Items/UserGroupItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refiner
+{
+    class UserGroupItem
+    {
+        public string ID { get; set; }
+    }
+}
