dff49e87a8aa45ae2dd3af83db2c171c2d0836ea
alex855k <alexander2341@gmail.com>
2018-12-13 15:57:06 +0100

    (Ingestion-service/GraBID#12) Refactor refiner changes out of this branch.

--- Src/Refiner/Program.cs
+++ Src/Refiner/Program.cs
@@ -11,30 +11,87 @@ using System.Linq;
 using System.Runtime.Loader;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Refiner
 {
     class Program
     {
+        private static IConnection conn;
+
+        private static IModel channel;
         private static readonly AutoResetEvent WaitHandle = new AutoResetEvent(false);
 
         static void Main(string[] args)
         {
+            AssemblyLoadContext.Default.Unloading += _ => Exit();
+            Console.CancelKeyPress += (_, __) => Exit();
+
             Log.Logger = new LoggerConfiguration()
                 .Enrich.WithProperty("name", typeof(Program).Assembly.GetName().Name)
                 .WriteTo.Console()
                 .CreateLogger();
 
             Log.Information("Starting...");
-            
-            RefinerService refiner = new RefinerService(Log.Logger);
-            
-            refiner.RetrieveMessageFromQueue("mono.data.received");
-            
+
+            var factory = new ConnectionFactory() { HostName = "rabbit.docker" };
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+
+            var exchange = "grabid_exchange";
+            var routingKey = "mono.data.received";
+
+            channel.ExchangeDeclare(exchange: exchange, type: "topic");
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queue: queueName,
+                                  exchange: exchange,
+                                  routingKey: routingKey);
+
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                var key = ea.RoutingKey;
+                Console.WriteLine($" [x] Received '{key}':'{message}'");
+                var envelope = JsonConvert.DeserializeObject<Envelope<string>>(message);
+
+                dynamic json = JObject.Parse(envelope.Payload);
+                string messageString = json.message;
+                string userString = json.users;
+                string[] userArray = userString.Split(",");
+                string[] messages = userArray.Select(user => { return $"{messageString} {user}"; }).ToArray();
+                var returnEnvelope = new Envelope<string[]>(envelope.Id, messages);
+                string newMessage = JsonConvert.SerializeObject(returnEnvelope);
+                byte[] messageBodyBytes = Encoding.UTF8.GetBytes(newMessage);
+
+                channel.BasicPublish(exchange, "mono.data.refined", null, messageBodyBytes);
+                Console.WriteLine($" [x] Sent 'mono.data.refined':'{newMessage}'");
+
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
             Log.Information("Started");
 
             WaitHandle.WaitOne();
         }
+
+        private static void Exit()
+        {
+            Log.Information("Exiting...");
+            channel.Close();
+            conn.Close();
+
+           
+        }
+
+       
     }
+
 }
