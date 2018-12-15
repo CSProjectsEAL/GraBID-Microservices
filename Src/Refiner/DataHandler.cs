e9e80f21b92397c76ce4b8206cd4f61384c36d91
alex855k <alexander2341@gmail.com>
2018-12-15 11:53:43 +0100

    (Refiner-service-CSProjectsEAL/GraBID#11) Add datahandler with logic with steps on how to process specific types of data.

---
+++ Src/Refiner/DataHandler.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refiner
+{
+    public class DataHandler
+    {
+        private IDataProcessor _processor;
+        private IDBFacade _db;
+        public string Destination { get; private set; }
+        public string CollectionName { get; private set; }
+        public string Database { get; private set; }
+        public DataHandler(IDataProcessor processor, string destination, IDBFacade db = null)
+        {
+            _processor = processor;
+            _db = db;
+        }
+
+        public void Handle(string data)
+        {
+            string processedData = _processor.Process(data);
+
+            if (_db != null) {
+                _db.SaveCollection(processedData, CollectionName, Database);
+            }
+        }
+
+    }
+}
