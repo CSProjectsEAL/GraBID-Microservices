c4167e8ca637377f0da857e66a063f43aa642fb5
alex855k <alexander2341@gmail.com>
2018-12-15 11:54:38 +0100

    (Refiner-service-CSProjectsEAL/GraBID#11) Fix name for processor and add interface.

---
+++ Src/Refiner/ExtractSubscribersProcessor.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json.Linq;
+using Shared;
+
+namespace Refiner
+{
+    public class SubscriptionDataProcessor : IDataProcessor
+    {
+        // Clean data by extracting what it needs
+        public string Process(string data)
+        {
+            dynamic processedData = new JArray();
+            dynamic envelopeData = JToken.Parse(data);
+
+            foreach (var obj in envelopeData)
+            {
+                dynamic entry = new JObject();
+
+                entry.Reseller = new JArray();
+                entry.User = new JArray();
+                entry.Subscription = new JArray();
+                entry.Bill = new JArray();
+                entry.UserGroup = new JArray();
+
+                entry.UserGroup = obj.
+                entry.Reseller.Id = obj.resellerId;
+                entry.Reseller.Name = obj.resellerName;
+                entry.Reseller.Discount = obj.discount;
+
+                entry.User.AccountId = obj.accountId;
+
+                entry.Subscription.Name = obj.subscriptionName;
+                entry.Subscription.Type = obj.type;
+                entry.Subscription.State = obj.state;
+                entry.Subscription.Price = obj.price;
+                entry.Subscription.OnExpiry = obj.onExpiry;
+                entry.Subscription.StartDate = obj.startDate;
+                entry.Subscription.ExpiryDate = obj.expiryDate;
+
+                entry.Bill.BillingDate = obj.billingDate;
+                entry.DiscountedPrice = obj.discountedPrice;
+                entry.CurrencyName = obj.currencyName;
+                entry.CurrencyISO = obj.code;
+
+                processedData.Add(entry);
+            }
+            return processedData;
+        }
+    }
+}
